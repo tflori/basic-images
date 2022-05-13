bacba291adda9111fce2fa273b72d536c97837ce
Thomas Flori <thflori@gmail.com>
2022-05-13 09:08:26 +0200

    upgrade ci to use gitlab-ci

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,53 +1,47 @@
 #!/bin/bash
 set -e
 
-if $CI; then
-    echo "Running on travis ci..."
-fi
-
-IMAGE="$TRAVIS_BRANCH$1"
+IMAGE="$1"
+VERSION={$2-latest}
 NAMESPACE="iras"
 
-if [ $IMAGE == "master" ]; then
-    echo "nothing to do for master..."
-    exit 0;
+if [ -z $IMAGE ]; then
+    echo "please provide an image name to build"
+    exit 1;
 fi
 
 if [[ $IMAGE =~ -[0-9.]*$ ]]; then
     VERSION=$(echo "$IMAGE" | sed 's/^.*-\([0-9.]*\)$/\1/')
     IMAGE=$(echo "$IMAGE" | sed 's/^\(.*\)-[0-9.]*$/\1/')
-else
-    VERSION=latest
 fi
 
-if [ -z $TRAVIS_TAG ]; then
-    # non tags are always latest versions
-    VERSION=latest
-elif [[ $VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]$ ]]; then
+if [[ $VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]$ ]]; then
     # version major.minor.fix gets major.minor on docker
     VERSION=$(echo "$VERSION" | cut -d "." -f 1,2)
 fi
-
-echo "Image: $IMAGE"
-echo "Version: $VERSION"
-echo "Namespace: $NAMESPACE"
-echo "Username: $DOCKER_USERNAME"
 
 if [ ! -d $IMAGE ]; then
     echo "$IMAGE does not exist" >&2
     exit 1;
 fi
 
+echo "Image: $IMAGE"
+echo "Version: $VERSION"
+echo "Namespace: $NAMESPACE"
+
 set -x
 
 docker build -t $NAMESPACE/$IMAGE:$VERSION $IMAGE
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $NAMESPACE/$IMAGE:$VERSION
+
+set +x
 
 if [[ $VERSION =~ ^[0-9]*\.[0-9]*$ ]]; then
     MAJORVERSION=$(echo "$VERSION" | cut -d "." -f 1)
+    set +x
     docker tag $NAMESPACE/$IMAGE:$VERSION $NAMESPACE/$IMAGE:$MAJORVERSION
     docker tag $NAMESPACE/$IMAGE:$VERSION $NAMESPACE/$IMAGE:latest
     docker push $NAMESPACE/$IMAGE:$MAJORVERSION
     docker push $NAMESPACE/$IMAGE:latest
+    set -x
 fi
